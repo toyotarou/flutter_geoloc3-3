9e6dead966929e1fddb75457afc73eed758f4c3f
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-02-22 18:48:09 +0900

    commit 20250222

--- lib/screens/components/daily_geoloc_display_alert.dart
+++ lib/screens/components/daily_geoloc_display_alert.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 import '../../collections/geoloc.dart';
@@ -40,34 +38,15 @@ class _DailyGeolocDisplayAlertState extends State<DailyGeolocDisplayAlert> {
 
   bool isLoading = false;
 
-  bool isGeolocMapCreated = false;
-
-  Timer? timer;
-
   ///
-  @override
-  void initState() {
-    super.initState();
-
-    _makeGeolocList();
-
-    timer = Timer.periodic(
-      const Duration(milliseconds: 500),
-      (Timer timer) {
-        if (geolocMap.isNotEmpty) {
-          if (mounted) {
-            setState(() => isGeolocMapCreated = true);
-          }
-
-          timer.cancel();
-        }
-      },
-    );
-  }
+  void _init() => _makeGeolocList();
 
   ///
   @override
   Widget build(BuildContext context) {
+    // ignore: always_specify_types
+    Future(_init);
+
     makeDiffSeconds();
 
     makePickupGeolocList();
@@ -90,45 +69,41 @@ class _DailyGeolocDisplayAlertState extends State<DailyGeolocDisplayAlert> {
                     Row(
                       children: <Widget>[
                         GestureDetector(
-                          onTap: isGeolocMapCreated
-                              ? () async {
-                                  bool errFlg = false;
-                                  String contentStr = '';
+                          onTap: () async {
+                            bool errFlg = false;
+                            String contentStr = '';
 
-                                  if (widget.geolocStateList.isEmpty) {
-                                    errFlg = true;
-                                    contentStr = 'mysqlデータがありません。';
-                                  }
+                            if (widget.geolocStateList.isEmpty) {
+                              errFlg = true;
+                              contentStr = 'mysqlデータがありません。';
+                            }
 
-                                  if (geolocMap.isEmpty) {
-                                    errFlg = true;
-                                    contentStr = 'geolocMapが作成されていません。';
-                                  }
+                            if (geolocMap.isEmpty) {
+                              errFlg = true;
+                              contentStr = 'geolocMapが作成されていません。';
+                            }
 
-                                  if (errFlg) {
-                                    // ignore: always_specify_types
-                                    Future.delayed(
-                                      Duration.zero,
-                                      () => error_dialog(
-                                          // ignore: use_build_context_synchronously
-                                          context: context,
-                                          title: 'isarデータを削除できません。',
-                                          content: contentStr),
-                                    );
+                            if (errFlg) {
+                              // ignore: always_specify_types
+                              Future.delayed(
+                                Duration.zero,
+                                () => error_dialog(
+                                    // ignore: use_build_context_synchronously
+                                    context: context,
+                                    title: 'isarデータを削除できません。',
+                                    content: contentStr),
+                              );
 
-                                    return;
-                                  }
+                              return;
+                            }
 
-                                  _showDeleteDialog(geolocList: geolocMap[widget.date.yyyymmdd]);
-                                }
-                              : null,
+                            _showDeleteDialog(geolocList: geolocMap[widget.date.yyyymmdd]);
+                          },
                           child: Column(
                             children: <Widget>[
                               const Text('delete'),
                               Icon(Icons.delete,
-                                  color: (widget.geolocStateList.isEmpty || !isGeolocMapCreated)
-                                      ? Colors.grey
-                                      : Colors.lightBlueAccent),
+                                  color: (widget.geolocStateList.isEmpty) ? Colors.grey : Colors.lightBlueAccent),
                               const Text('isar'),
                             ],
                           ),
