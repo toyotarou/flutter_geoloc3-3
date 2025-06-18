2479f0a1004f2fd75e794ee66cc821f0a42e98c6
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-06-18 10:29:08 +0900

    commit 20250618 kotlin hello

--- android/app/src/main/kotlin/com/example/flutter_geoloc3/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_geoloc3/MainActivity.kt
@@ -1,5 +1,15 @@
 package com.example.flutter_geoloc3
 
 import io.flutter.embedding.android.FlutterActivity
+import android.content.Intent
+import android.os.Bundle
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Flutter を起動せず Kotlin 画面に遷移
+        val intent = Intent(this, WifiLocationActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
